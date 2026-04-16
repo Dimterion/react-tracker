b0aba23b834fb5f07c9b36b5f159c460ed0cd621
Dmitrii <dimterion59@gmail.com>
2026-04-16 15:01:15 +0200

    add basic routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import HomeHeader from "@/components/HomeHeader";
 import { globalStyles } from "@/styles/global";
 import * as Device from "expo-device";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Platform, ScrollView, StyleSheet, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <HomeHeader />
+      <Link href="/tracking">Tracking</Link>
       <Text style={styles.title}>Welcome.</Text>
       <Text style={{ backgroundColor: "red", marginTop: 30 }}>
         React Tracker: {Platform.OS}
@@ -14,7 +16,7 @@ export default function HomeScreen() {
       <Text>React Tracker: {Device.modelName}</Text>
       <Text>React Tracker: {Device.brand}</Text>
       <Text>React Tracker: {Device.osVersion}</Text>
-    </View>
+    </ScrollView>
   );
 }
 
---
+++ src/app/tracking.tsx
@@ -0,0 +1,10 @@
+import { globalStyles } from "@/styles/global";
+import { ScrollView, Text } from "react-native";
+
+export default function TrackingScreen() {
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text>Tracking Screen</Text>
+    </ScrollView>
+  );
+}
