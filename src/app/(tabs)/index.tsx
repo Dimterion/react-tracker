25ce3b41d1bc8c6c3c8da869cc2fdf15814c8d7b
Dmitrii <dimterion59@gmail.com>
2026-04-17 11:25:53 +0200

    replace header with tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
+import HomeHeader from "@/components/HomeHeader";
 import { globalStyles } from "@/styles/global";
 import { Link } from "expo-router";
 import { ScrollView, Text } from "react-native";
-import HomeHeader from "../components/HomeHeader";
 
 export default function HomeScreen() {
   return (
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,9 @@
-import { colors } from "@/styles/global";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: colors.header },
-        headerTintColor: "#03045e",
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false, title: "Home" }}
-      />
-      <Stack.Screen options={{ title: "Tracking" }} name="tracking" />
-      <Stack.Screen options={{ title: "Add Tracker" }} name="add-tracker" />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
