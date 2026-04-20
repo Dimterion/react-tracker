7f3f2b93cbab1f73706e58f6359cb6f70a871c5f
Dmitrii <dimterion59@gmail.com>
2026-04-20 09:40:03 +0200

    add TrackingScreen functionality

--- src/app/(tabs)/tracking.tsx
+++ src/app/(tabs)/tracking.tsx
@@ -1,10 +1,45 @@
+import TrackItem from "@/components/TrackingItem";
+import { getTracks, Track } from "@/storage/tracks";
 import { globalStyles } from "@/styles/global";
-import { ScrollView, Text } from "react-native";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
 export default function TrackingScreen() {
+  const [tracks, setTracks] = useState<Track[]>([]);
+
+  const loadTracks = async () => {
+    const data = await getTracks();
+    setTracks(data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadTracks();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>Tracking</Text>
+      <View style={{ marginTop: 30 }}>
+        {tracks.length === 0 ? (
+          <Text style={globalStyles.empty}>No tracks logged yet.</Text>
+        ) : (
+          tracks.map((track) => (
+            <TrackItem
+              key={track.id}
+              id={track.id}
+              name={track.name}
+              el1={track.el1}
+              el2={track.el2}
+              el3={track.el3}
+              el4={track.el4}
+              onDelete={loadTracks}
+            />
+          ))
+        )}
+      </View>
     </ScrollView>
   );
 }
