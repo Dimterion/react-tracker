40039244fc6d7476239bf07ce34b326442f120bf
Dmitrii <dimterion59@gmail.com>
2026-04-27 10:13:08 +0200

    adjust add-link screen

--- app/(tabs)/add-link.tsx
+++ app/(tabs)/add-link.tsx
@@ -1,10 +1,75 @@
-import { globalStyles } from "@/styles/global";
-import { Text, View } from "react-native";
+import { colors, globalStyles } from "@/styles/global";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function AddLinkScreen() {
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+
+  const handleAddLink = () => {
+    console.log({ name, link });
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Add Link</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Link name"
+        placeholderTextColor={colors.textSecondary}
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Link"
+        placeholderTextColor={colors.textSecondary}
+        keyboardType="numeric"
+        value={link}
+        onChangeText={setLink}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleAddLink}>
+        <Text style={styles.buttonText}>Add Link</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: colors.surface,
+    color: colors.text,
+    padding: 16,
+    borderRadius: 10,
+    fontSize: 16,
+    marginTop: 16,
+  },
+  row: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  rowInput: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: colors.primary,
+    padding: 16,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 24,
+  },
+  buttonText: {
+    color: colors.background,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
