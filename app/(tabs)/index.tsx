d5e36c0b2a489bbad2f8ee37f2b63fe84c72c0e4
Dmitrii <dimterion59@gmail.com>
2026-04-28 11:48:59 +0200

    adjust home screen for stored links func

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,27 @@
 import Header from "@/components/Header";
 import LinkGrid from "@/components/LinkGrid";
 import RecentLinks from "@/components/RecentLinks";
+import { getLinks, Link } from "@/storage/links";
 import { globalStyles } from "@/styles/global";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
+  const [links, setLinks] = useState<Link[]>([]);
+
+  const loadLinks = async () => {
+    const data = await getLinks();
+    setLinks(data);
+    console.log("Loaded links:", data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadLinks();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>React Native Playground</Text>
