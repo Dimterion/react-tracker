72e484fdcf54f5637c5794db17c14c6a289fd650
Dmitrii <dimterion59@gmail.com>
2026-04-30 09:28:10 +0200

    add ShareButton comp

---
+++ components/ShareButton.tsx
@@ -0,0 +1,29 @@
+import { Link } from "@/storage/links";
+import { colors } from "@/styles/global";
+import { Ionicons } from "@expo/vector-icons";
+import { Share, TouchableOpacity } from "react-native";
+
+type ShareButtonProps = {
+  links: Link[];
+};
+
+export default function ShareButton({ links }: ShareButtonProps) {
+  const handleShare = async () => {
+    const totals = links.reduce(
+      (acc, link) => ({
+        reglink: acc.reglink + link.reglink,
+      }),
+      { reglink: 0 },
+    );
+
+    await Share.share({
+      message: `Summary\n\nLinks: ${totals.reglink} logged.`,
+    });
+  };
+
+  return (
+    <TouchableOpacity onPress={handleShare}>
+      <Ionicons name="share-outline" size={24} color={colors.primary} />
+    </TouchableOpacity>
+  );
+}
