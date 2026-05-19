b9c8ff439ddb274fcb93967efc337a54f026c512
Dmitrii <dimterion59@gmail.com>
2026-05-19 10:52:35 +0200

    adjust workouts layout header

--- app/(tabs)/workouts/_layout.tsx
+++ app/(tabs)/workouts/_layout.tsx
@@ -1,8 +1,25 @@
+import { colors } from "@/styles/global";
 import { Stack } from "expo-router";
 
 export default function WorkoutsLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.surface,
+        },
+        headerTintColor: colors.primary,
+        headerTitleStyle: {
+          color: colors.text,
+          fontWeight: "700",
+        },
+        headerShadowVisible: false,
+        headerBackButtonDisplayMode: "minimal",
+        contentStyle: {
+          backgroundColor: colors.background,
+        },
+      }}
+    >
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="new" options={{ title: "New Workout" }} />
       <Stack.Screen name="[id]" options={{ title: "Workout Details" }} />
